c6f36739c05569df45d3bb68833a941905ec13ab
Ahmed Abdelgawad <hagahm721@gmail.com>
2025-11-17 14:50:11 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+
+// ignore_for_file: camel_case_types
+
+import 'package:flutter/material.dart';
+
+void main()
+{
+  runApp(Responsive_Dashboard());
+}
+
+class Responsive_Dashboard extends StatelessWidget {
+  const Responsive_Dashboard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+      
+        )
+    );
+  }
+}
+
